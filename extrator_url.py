24f1760a11395ddbf029b093bdf10bef4756a24e
Kayke <alveskayke033@gmail.com>
2021-07-06 21:05:32 -0300

    extrator URL

--- extrator_url.py
+++ extrator_url.py
@@ -56,5 +56,5 @@ extrator_url = Extrator_url(url)
 print(f'Tamanho da URL: ', len(extrator_url))
 print(extrator_url)
 
-valor_quantidade = extrator_url.get_valor_parametro('moedaDestino')
+valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
